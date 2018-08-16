762ae94400bcd7d7be3acb28c6df0a2dabed053b
D0109 <sq.luo@siid.ac.cn>
2018-08-16 18:13:58 +0800

    策略管理

--- src/app/home/strategy/strategy.component.ts
+++ src/app/home/strategy/strategy.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StrategyComponent implements OnInit {
 
+  navs = [{
+    id : 0,
+    name: '策略时间'
+    },
+    {
+      id: 1,
+      name: '策略范围'
+    }
+  ];
+  index = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeNav(index) {
+    this.index = index;
   }
 
 }
