e4fb49a4b046ebfff6fa331e7a226430d0544004
D0109 <sq.luo@siid.ac.cn>
2018-08-17 15:44:32 +0800

    策略管理-策略范围

--- src/app/home/strategy/strategy.component.ts
+++ src/app/home/strategy/strategy.component.ts
@@ -1,8 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+/*
+
+Copyright(c): 2018 深圳创新设计研究院
+Author: luo.shuqi@live.com
+@file: strategy.componen.ts
+@time: 2018 /8 / 16 9: 00
+
+*/
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-// import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { VideoService } from '../../service/video.service';
 
 declare let echarts;
+declare let BMap;
+declare let $: any;
 @Component({
   selector: 'app-strategy',
   templateUrl: './strategy.component.html',
@@ -10,6 +20,8 @@ declare let echarts;
 })
 
 export class StrategyComponent implements OnInit {
+
+  @ViewChild('map2') map_container: ElementRef;
 
   navs = [{
     id : 0,
@@ -36,9 +48,27 @@ export class StrategyComponent implements OnInit {
   // 弹框
   closeResult: string;
   strategy_index: number; // 策略索引
+  model: any = {}; // 存储数据
+  map: any; // 地图
+  zTreeObj: any;
 
-// , public activeModal: NgbActiveModal
-  constructor(private modalService: NgbModal) { }
+  // zTree 的数据属性，深入使用请参考 API 文档（zTreeNode 节点数据详解）
+  zNodes: any;
+  city = '广州市'; // 当前选中城市
+
+  public zTreeOnClick: (event, treeId, treeNode) => void;
+  constructor(private modalService: NgbModal, private videoService: VideoService) {
+    // 树的操作
+    // 点击
+    const that = this;
+    this.zTreeOnClick = (event, treeId, treeNode) => {
+      this.city = treeNode.full_name;
+      console.log(treeNode.tId + ', ' + treeNode.full_name);
+      this.getPoint(that.map, that.city);
+
+    };
+
+  }
 
   ngOnInit() {
     setTimeout(() => {
@@ -161,6 +191,12 @@ export class StrategyComponent implements OnInit {
         this.chartMapChana1();
         this.chartMapChana2();
       }, 2);
+    } else {
+      setTimeout(() => {
+        this.getZoneDefault();
+        this.addBeiduMap();
+      }, 2);
+
     }
   }
 
@@ -173,6 +209,72 @@ export class StrategyComponent implements OnInit {
   removeStrategy() {
     const index = this.strategy_index;
     this.strategyList.splice(index, 1);
+  }
+
+  // 获取城市列表
+  getZoneDefault() {
+    const that = this;
+    const setting = {// zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
+      callback: {
+        onClick: this.zTreeOnClick
+      }
+    };
+    this.videoService.getZoneDefault()
+      .subscribe({
+        next: function (res) {
+          that.model.ZoneDefault = res;
+          that.zNodes = res.regions;
+          that.zTreeObj = $.fn.zTree.init($('#treeDemo'), setting, that.zNodes);
+        },
+        complete: function () {
+          console.log('that.zNodes!');
+          console.log(that.zNodes);
+        },
+        error: function (error) {
+          console.log(error);
+        }
+      });
+  }
+
+  // 百度地图API功能
+  addBeiduMap() {
+
+    const map = this.map = new BMap.Map(this.map_container.nativeElement, {
+      enableMapClick: true,
+      minZoom: 11,
+      // maxZoom : 11
+    }); // 创建地图实例
+
+
+    // 这里我们使用BMap命名空间下的Point类来创建一个坐标点。Point类描述了一个地理坐标点，其中116.404表示经度，39.915表示纬度。（为天安门坐标）
+    const point = new BMap.Point(114.064675, 22.550651); // 坐标可以通过百度地图坐标拾取器获取
+    map.centerAndZoom(point, 19); // 设置中心和地图显示级别
+
+    const marker = new BMap.Marker(point);  // 创建标注
+    map.addOverlay(marker);               // 将标注添加到地图中
+
+  }
+
+  // 解析地址- 设置中心和地图显示级别
+  getPoint(baiduMap, city) {
+    const that = this;
+    // 创建地址解析器实例
+    const myGeo = new BMap.Geocoder();
+    const name = city;
+    let pt;
+
+    // 将地址解析结果显示在地图上,并调整地图视野，获取数据-添加标注
+    myGeo.getPoint(name, function (point) {
+      if (point) {
+
+        baiduMap.centerAndZoom(point, 19);
+        pt = point;
+        const marker = new BMap.Marker(point);  // 创建标注
+        baiduMap.addOverlay(marker);
+      } else {
+        console.log('您选择地址没有解析到结果!');
+      }
+    }, '');
   }
 
 }
