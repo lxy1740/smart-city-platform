4e3a179ea514b47256901ee27716a4bb5725f9a7
D0109 <sq.luo@siid.ac.cn>
2018-08-17 17:41:20 +0800

    测试

--- src/app/home/strategy/strategy.component.ts
+++ src/app/home/strategy/strategy.component.ts
@@ -22,6 +22,7 @@ declare let $: any;
 export class StrategyComponent implements OnInit {
 
   @ViewChild('map2') map_container: ElementRef;
+  @ViewChild('map3') map_container3: ElementRef;
 
   navs = [{
     id : 0,
@@ -57,7 +58,7 @@ export class StrategyComponent implements OnInit {
   city = '广州市'; // 当前选中城市
 
   public zTreeOnClick: (event, treeId, treeNode) => void;
-  constructor(private modalService: NgbModal, private videoService: VideoService) {
+  constructor(private modalService: NgbModal, private videoService: VideoService, public element: ElementRef) {
     // 树的操作
     // 点击
     const that = this;
@@ -168,8 +169,24 @@ export class StrategyComponent implements OnInit {
       console.log(this.closeResult);
     });
   }
-  close() {
+  open2(content) {
 
+    const that = this;
+
+    const modal = this.modalService.open(content, { size: 'lg' });
+    setTimeout(() => {
+      console.log(this.element.nativeElement.querySelector('#map3'));
+
+    }, 2);
+    that.addBeiduMap2();
+    modal.result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      console.log(this.closeResult);
+
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      console.log(this.closeResult);
+    });
   }
 
 
@@ -240,6 +257,25 @@ export class StrategyComponent implements OnInit {
   addBeiduMap() {
 
     const map = this.map = new BMap.Map(this.map_container.nativeElement, {
+      enableMapClick: true,
+      minZoom: 11,
+      // maxZoom : 11
+    }); // 创建地图实例
+
+
+    // 这里我们使用BMap命名空间下的Point类来创建一个坐标点。Point类描述了一个地理坐标点，其中116.404表示经度，39.915表示纬度。（为天安门坐标）
+    const point = new BMap.Point(114.064675, 22.550651); // 坐标可以通过百度地图坐标拾取器获取
+    map.centerAndZoom(point, 19); // 设置中心和地图显示级别
+
+    const marker = new BMap.Marker(point);  // 创建标注
+    map.addOverlay(marker);               // 将标注添加到地图中
+
+  }
+
+  // 百度地图API功能
+  addBeiduMap2() {
+
+    const map = this.map = new BMap.Map('allmap', {
       enableMapClick: true,
       minZoom: 11,
       // maxZoom : 11
