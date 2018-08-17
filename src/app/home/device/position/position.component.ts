d6bb3e37b46a6815d5cf61d67cb4eac9147cab57
unknown <D1181@SIID.COM.CN>
2018-08-17 18:36:47 +0800

    style中添加俩modal样式；设备管理modal调整

--- src/app/home/device/position/position.component.ts
+++ src/app/home/device/position/position.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+
+// baidu map
+declare let BMap;
+declare let $: any;
+declare let BMapLib;
+declare let BMAP_ANCHOR_TOP_LEFT;
 
 @Component({
   selector: 'app-position',
@@ -7,23 +13,49 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./position.component.scss']
 })
 export class PositionComponent implements OnInit {
+  @ViewChild('map1') map_container: ElementRef;
   closeResult: string;
+  map: any; // 地图对象
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    // this.addBaiduMap();
   }
-  open(content) {
+  openNewPosition(content) {
     const that = this;
-    this.modalService.open(content, { size: 'lg' }).result.then((result) => {
+
+    const modal = this.modalService.open( content, {windowClass: 'ex-lg-modal' });
+    this.addBaiduMap();
+    modal.result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
+
       console.log(this.closeResult);
 
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       console.log(this.closeResult);
     });
+    // setTimeout(() => {
+    //   that.addBaiduMap();
+    // }, 1000);
   }
+
+  openAddPositions(content) {
+    const that = this;
+
+    this.modalService.open(content, { size: 'lg'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+
+      console.log(this.closeResult);
+
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      console.log(this.closeResult);
+    });
+
+  }
+
 
   private getDismissReason(reason: any): string {
     if (reason === ModalDismissReasons.ESC) {
@@ -33,6 +65,16 @@ export class PositionComponent implements OnInit {
     } else {
       return `with: ${reason}`;
     }
+  }
+  addBaiduMap() {
+    const map = this.map = new BMap.Map('position_map', {
+      enableMapClick: true,
+      // minZoom: 11
+    }); // 创建地图实例
+    const point = new BMap.Point(113.922329, 22.49656); // 坐标可以通过百度地图坐标拾取器获取 --万融大厦
+    map.centerAndZoom(point, 19); // 设置中心和地图显示级别
+    map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+    map.setMapStyle({ style: 'normal' });
   }
 
 }
--- src/app/home/device/survey/survey.component.ts
+++ src/app/home/device/survey/survey.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
+declare let BMap;
+declare let $: any;
+declare let BMapLib;
+declare let BMAP_ANCHOR_TOP_LEFT;
+
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
@@ -8,14 +13,16 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 })
 export class SurveyComponent implements OnInit {
   closeResult: string;
+  map: any; // 地图对象
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    
   }
-  open(content) {
+  openAddSurveys(content) {  //批量导入
     const that = this;
-    this.modalService.open(content, { size: 'lg' }).result.then((result) => {
+    this.modalService.open(content, {windowClass: 'md-modal'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
       console.log(this.closeResult);
 
@@ -23,6 +30,31 @@ export class SurveyComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       console.log(this.closeResult);
     });
+  }
+  openNewSurvey(content) { //新建设备
+    const that = this;
+    const modal = this.modalService.open( content, {windowClass: 'ex-lg-modal' });
+    this.addBaiduMap();
+    modal.result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+
+      console.log(this.closeResult);
+
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      console.log(this.closeResult);
+    });
+  }
+
+  addBaiduMap() {
+    const map = this.map = new BMap.Map('survey_map', {
+      enableMapClick: true,
+      // minZoom: 11
+    }); // 创建地图实例
+    const point = new BMap.Point(113.922329, 22.49656); // 坐标可以通过百度地图坐标拾取器获取 --万融大厦
+    map.centerAndZoom(point, 19); // 设置中心和地图显示级别
+    map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+    map.setMapStyle({ style: 'normal' });
   }
 
   private getDismissReason(reason: any): string {
@@ -34,5 +66,6 @@ export class SurveyComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
+
 
 }
