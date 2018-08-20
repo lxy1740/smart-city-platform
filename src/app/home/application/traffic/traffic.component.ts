b56eb3d38b1cbd1f7cc7afc2cacd768c3da2344a
unknown <D1185@SIID.COM.CN>
2018-08-20 18:04:10 +0800

    智慧交通添加地图区域选择

--- src/app/home/application/traffic/traffic.component.ts
+++ src/app/home/application/traffic/traffic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { MonitorService } from '../../../service/monitor.service';
 // baidu map
 declare let BMap;
 declare let $: any;
@@ -19,10 +21,26 @@ export class TrafficComponent implements OnInit {
   model: any = {}; // 存储数据
 
   map: any; // 地图对象
-  constructor() { }
+
+  cityList: any; // 城市列表
+  deviceList: any; // 城市列表
+  defaultZone: any; // 默认城市
+  currentCity: any; // 当前城市
+  currentChildren: any; // 当前城市节点
+  currentBlock: any; // // 当前城市街道
+
+  visible = true; // 控制可视区域
+  areashow = false; // 默认区域列表不显示
+  cityshow = false; // 默认区域列表不显示
+  zoom: any; // 地图级数
+
+  parentNode = null; // 用于递归查询JSON树 父子节点
+  node = null; // 用于递归查询JSON树 父子节点
+  constructor(private monitorService: MonitorService, public router: Router, ) { }
 
   ngOnInit() {
     this.addBeiduMap();
+    this.getCity(); // 获取城市列表
   }
 
   // 百度地图API功能
@@ -64,6 +82,168 @@ export class TrafficComponent implements OnInit {
 
   }
 
+  // 获取城市列表 --ok
+  getCity() {
+    const that = this;
+
+    this.monitorService.getZoneDefault().subscribe({
+      next: function (val) {
+        that.cityList = val.regions;
+        that.currentCity = val.zone;
+        that.zoom = that.switchZone(val.zone.level);
+        that.node = that.getNode(val.regions, val.zone.region_id);
+        that.currentChildren = that.node.children;
+
+      },
+      complete: function () {
+        that.addBeiduMap(); // 创建地图
+
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  }
+  // 省市区街道-地图级别
+  switchZone(level) {
+    let zone = 12;
+    switch (level) {
+      case 1:
+        zone = 10;
+        break;
+      case 2:
+        zone = 12;
+        break;
+      case 3:
+        zone = 15;
+        break;
+      case 4:
+        zone = 19;
+        break;
+      default:
+        break;
+    }
+    return zone;
+  }
+
+  getNode(json, nodeId) {
+    const that = this;
+
+    // 1.第一层 root 深度遍历整个JSON
+    for (let i = 0; i < json.length; i++) {
+      if (that.node) {
+        break;
+      }
+
+      const obj = json[i];
+
+      // 没有就下一个
+      if (!obj || !obj.id) {
+        continue;
+      }
+      // console.log(nodeId);
+      // console.log(obj.id);
+      // 2.有节点就开始找，一直递归下去
+      if (obj.id === nodeId) {
+        // 找到了与nodeId匹配的节点，结束递归
+        that.node = obj;
+
+        break;
+      } else {
+
+        // 3.如果有子节点就开始找
+        if (obj.children) {
+          // 4.递归前，记录当前节点，作为parent 父亲
+          that.parentNode = obj;
+
+          // 递归往下找
+          that.getNode(obj.children, nodeId);
+        } else {
+          // 跳出当前递归，返回上层递归
+          continue;
+        }
+      }
+    }
+
+
+    // 5.如果木有找到父节点，置为null，因为没有父亲
+    if (!that.node) {
+      that.parentNode = null;
+    }
+
+    // 6.返回结果obj
+    // return {
+    //   parentNode: that.parentNode,
+    //   node: that.node
+    // };
+    return that.node;
+  }
+
+
+
+  // 解析地址- 设置中心和地图显示级别
+  getPoint(baiduMap, city) {
+    const that = this;
+    // 创建地址解析器实例
+    const myGeo = new BMap.Geocoder();
+    const zoom = this.zoom = this.switchZone(city.level);
+    const fullName = city.full_name;
+    console.log(city);
+
+    let pt;
+
+    // 将地址解析结果显示在地图上,并调整地图视野，获取数据-添加标注
+    myGeo.getPoint(fullName, function (point) {
+      if (point) {
+        baiduMap.centerAndZoom(point, zoom);
+        pt = point;
+
+      } else {
+        console.log('您选择地址没有解析到结果!');
+      }
+    }, '');
+  }
+  // 选择区域
+  // 选择城市
+  selecteCity(city) {
+    this.currentCity = city;
+    this.node = city;
+    this.getPoint(this.map, city);  // 解析地址- 设置中心和地图显示级别
+    this.currentChildren = city.children;
+  }
+
+  selecteblock(block) {
+    this.getPoint(this.map, block);  // 解析地址- 设置中心和地图显示级别
+  }
+
+  // 显示区域
+  showArea() {
+    this.areashow = true;
+  }
+  // 显示城市
+  showCiyt() {
+    this.cityshow = true;
+  }
+// 离开城市
+citylistMouseleave() {
+  this.cityshow = false;
+}
+// 选择区域
+arealistMouseover(area) {
+
+  this.currentBlock = area.children;
+}
+
+// 离开区域
+arealistMouseleave() {
+  this.areashow = false;
+  this.currentBlock = null;
+}
+
+arealistMouseNone() {
+  this.areashow = true;
+  this.currentBlock = null;
+}
 }
 
 /*
