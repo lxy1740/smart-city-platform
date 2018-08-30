cd81544b0e2d777c12ddbaf29ce146be17d339ac
D0109 <sq.luo@siid.ac.cn>
2018-08-30 17:02:35 +0800

    时间筛选搜索数据

--- src/app/home/airreport/airreport.component.ts
+++ src/app/home/airreport/airreport.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTimepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { AirmonitorService } from '../../service/airmonitor.service';
-// import { Point } from '../../data/point.type';
+
+const now = new Date();
 
 @Component({
   selector: 'app-airreport',
@@ -15,16 +19,28 @@ export class AirreportComponent implements OnInit {
   NorthEast: any; // 坐标点
   SouthWest: any; // 坐标点
   closeResult: string;
-  historydatalist: any; // 指定设备的历史数据集合
+  historydatalist = []; // 指定设备的历史数据集合
+  historydatalistItems: any;
   deviceIndx = 0;
+  total: any;
 
   page = 1;
 
+  startDate: NgbDateStruct = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() }; // 开始日期
+  endDate: NgbDateStruct = { year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate() }; // 开始日期
+
+
+  startTime: NgbTimeStruct = { hour: 0, minute: 0, second: 0 };
+  endTime: NgbTimeStruct = { hour: 23, minute: 59, second: 59 };
+  seconds = true;
   constructor(private modalService: NgbModal, public router: Router,
-    private airmonitorService: AirmonitorService) {
+    private airmonitorService: AirmonitorService, config: NgbTimepickerConfig) {
     // 获取坐标范围
     this.NorthEast = JSON.parse(localStorage.getItem('NE'));
     this.SouthWest = JSON.parse(localStorage.getItem('SW'));
+
+    config.spinners = false; // 时间控制
+
   }
 
   ngOnInit() {
@@ -58,13 +74,32 @@ export class AirreportComponent implements OnInit {
   // 获取指定设备的历史数据记录
   getHistoryData() {
     const that = this;
-    const fromdate = '2018-01-01T00:00';
-    const todate = '2018-12-30T23:59';
+    const fromdate1 = '2018-01-01T00:00';
+    const todate1 = '2018-12-30T23:59';
+
+    const smonth = this.startDate.month.toString().length > 1 ? this.startDate.month.toString() : `0${this.startDate.month.toString()}`;
+    const sday = this.startDate.day.toString().length > 1 ? this.startDate.day.toString() : `0${this.startDate.day.toString()}`;
+
+    const shour = this.startTime.hour.toString().length > 1 ? this.startTime.hour.toString() : `0${this.startTime.hour.toString()}`;
+    const sminute = this.startTime.minute.toString().length > 1 ? this.startTime.minute.toString() : `0${this.startTime.minute.toString()}`;
+
+    const emonth = this.endDate.month.toString().length > 1 ? this.endDate.month.toString() : `0${this.endDate.month.toString()}`;
+    const eday = this.endDate.day.toString().length > 1 ? this.endDate.day.toString() : `0${this.endDate.day.toString()}`;
+
+    const ehour = this.endTime.hour.toString().length > 1 ? this.endTime.hour.toString() : `0${this.endTime.hour.toString()}`;
+    const eminute = this.endTime.minute.toString().length > 1 ? this.endTime.minute.toString() : `0${this.endTime.minute.toString()}`;
+
+    const fromdate =
+      `${this.startDate.year}-${smonth}-${sday}T${shour}:${sminute}`;
+
+    const todate = `${this.endDate.year}-${emonth}-${eday}T${ehour}:${eminute}`;
     const page = this.page;
     const pageSize = 10;
     this.airmonitorService.getHistoryData(this.currentdevice.id, fromdate, todate, page, pageSize).subscribe({
       next: function (val) {
         that.historydatalist = val;
+        that.historydatalistItems = val.items;
+        that.total = val.total;
         console.log(that.historydatalist);
       },
       complete: function () {
@@ -77,6 +112,11 @@ export class AirreportComponent implements OnInit {
 
   // 分页获取数据
   pageChange() {
+    this.getHistoryData();
+  }
+
+  // 搜索
+  dataSearch() {
     this.getHistoryData();
   }
 
