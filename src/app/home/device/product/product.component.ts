6aaf91c3ccf7f19df7bca6b354dcf595850a7d8f
D0109 <sq.luo@siid.ac.cn>
2018-09-13 12:46:19 +0800

    产品列表

--- src/app/home/device/product/product.component.ts
+++ src/app/home/device/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../../../service/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,11 +8,80 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  model: any = {};
   closeResult: string;
+  productList: any;
+  productListItems = [];
+  total: number; // 分页
+  page: number;
+  pagesize = 10;
+  type = 0;
+  deviceList = []; // 设备列表
+  deviceTypes = []; // 设备列表
+  currentType: any; // 当前搜索设备类别
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal,  private productService: ProductService) { 
+    this.page = 1;
+  }
 
   ngOnInit() {
+    this.getDevice();
+    this.getModel(0, this.page, this.pagesize);
+  }
+
+    // 获取设备型号
+  getModel(type: number, page: number, pagesize: number) {
+    const that = this;
+    this.productService.getModel(type, page, pagesize).subscribe({
+      next: function (val) {
+        that.productList = val;
+        that.total = val.total;
+        that.productListItems = val.items;
+
+      },
+      complete: function () {
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  }
+  // fenye
+  pageChange() {
+    this.getModel(this.currentType.id, this.page, this.pagesize);
+  }
+  // 获取设备列表
+  getDevice() {
+    const that = this;
+    this.productService.getDevice().subscribe({
+      next: function (val) {
+        that.deviceList = val;
+        that.model.device = val[0];
+        that.deviceTypes = val.map((item) => Object.assign({}, item));
+        that.deviceTypes.unshift({ id: 0, name: '不限' }); // 所有项
+        that.currentType = that.deviceTypes[0];
+      },
+      complete: function () {
+
+      },
+      error: function (error) {
+        console.log(error);
+      }
+    });
+  }
+
+
+  deviceTypeChange() {
+    this.getModel(this.currentType.id, this.page, this.pagesize);
+  }
+  changeName(modelId) {
+    let modelName;
+    this.deviceTypes.map((item, i) => {
+      if (item.id === modelId) {
+        modelName = item.name;
+      }
+    });
+    return modelName;
   }
   openNewProduct(content) {
     const that = this;
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -20,14 +20,37 @@ export class ProductService {
             .pipe(map((res: Response) => {
                 if (res.status === 200) {
                     const data = res.json();
-
-                    console.log(data.regions[0]);
                     data.regions[0].open = true;
-                    // data.regions[0].children[0].open = true;
-                    // data.regions[0].children.map((item, index) => {
-                    //     data.regions[0].children[index].open = true;
-                    // });
 
+                    return data;
+                } else if (res.status === 202) {
+                    return res.json().code.toString();
+
+                }
+            }));
+    }
+
+    // 设备列表
+    getDevice(): Observable<any> {
+        return this.http.get('/api/device/type/all')
+            .pipe(map((res: Response) => {
+                if (res.status === 200) {
+                    const data = res.json();
+                    return data;
+                } else if (res.status === 202) {
+                    return res.json().code.toString();
+
+                }
+            }));
+
+    }
+
+    // 获取设备型号
+    getModel(type: number, page: number, pagesize: number): Observable<any> {
+        return this.http.get(`/api/device/model?type=${type}&page=${page}&pageSize=${pagesize}`)
+            .pipe(map((res: Response) => {
+                if (res.status === 200) {
+                    const data = res.json();
                     return data;
                 } else if (res.status === 202) {
                     return res.json().code.toString();
