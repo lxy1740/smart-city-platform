838c927218216370b9df0c698486b706567c9065
D1181 <ym.zhao@siid.com.cn>
2018-09-19 08:58:50 +0800

    bug修改

--- src/app/service/device.service.ts
+++ src/app/service/device.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable, query } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/';
 import { map } from 'rxjs/operators';
