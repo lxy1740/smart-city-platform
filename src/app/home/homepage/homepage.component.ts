6fe49b7687ad35a4b26089a3e3ebae8ba4445138
unknown <D1198@SIID.COM.CN>
2018-10-30 15:24:02 +0800

    gb

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -12,11 +12,24 @@ export class HomepageComponent implements OnInit {
   // routes = ROUTELIST;
 
   constructor(public router: Router, private el: ElementRef) { }
+
   setChange() {
+    const styApp = this.el.nativeElement.querySelector('.app');
     const sty = this.el.nativeElement.querySelector('.btn1');
-    sty.style.background = 'url(../../../assets/imgs/user-profile.png) no-repeat center';
-    this.el.nativeElement.querySelector('.pp1').remove();
-    this.el.nativeElement.querySelector('.pp2').remove();
+    if (styApp) {
+      sty.style.background = 'url(../../../assets/imgs/user-profile.png) no-repeat center';
+      this.el.nativeElement.querySelector('.pp').remove();
+    } else {
+      sty.style.background = '#45939D';
+      // sty.append(this.openSideBar(e));
+    }
+
+   }
+   openSideBar(e) {
+    let html = `<div class="col-md-12 content pp">`;
+    html = html + `<p><span><i class="fa  nav-icon"></i></span></p>`;
+    html = html + `<p class="app">APP下载</p>`;
+    html = html + `</div>`;
    }
 
   ngOnInit() {
@@ -30,4 +43,6 @@ export class HomepageComponent implements OnInit {
   goToChange() {
     this.setChange();
   }
+
+
 }
