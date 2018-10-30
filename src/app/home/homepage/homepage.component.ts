88ac2202c7657610b7aa6c6fc618f601bd8b68e4
unknown <D1198@SIID.COM.CN>
2018-10-30 14:22:24 +0800

    app下载

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 // import { ROUTELIST } from './route-list';
 import { Router } from '@angular/router';
 
@@ -11,14 +11,23 @@ export class HomepageComponent implements OnInit {
 
   // routes = ROUTELIST;
 
-  constructor(public router: Router, ) { }
+  constructor(public router: Router, private el: ElementRef) { }
+  setChange() {
+    const sty = this.el.nativeElement.querySelector('.btn1');
+    sty.style.background = 'url(../../../assets/imgs/user-profile.png) no-repeat center';
+    this.el.nativeElement.querySelector('.pp1').remove();
+    this.el.nativeElement.querySelector('.pp2').remove();
+   }
 
   ngOnInit() {
+    // this.setHeight();
   }
 
   goToZheRoute(para) {
     this.router.navigate([para]);
   }
 
-
+  goToChange() {
+    this.setChange();
+  }
 }
