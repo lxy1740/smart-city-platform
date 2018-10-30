39e040f93da735c44220cc4ee387169f16b66456
D0109 <sq.luo@siid.ac.cn>
2018-10-30 15:17:25 +0800

    led

--- src/app/home/led-test/led-test.component.ts
+++ src/app/home/led-test/led-test.component.ts
@@ -9,7 +9,7 @@ import { AirmonitorService } from '../../service/airmonitor.service';
   styleUrls: ['./led-test.component.scss']
 })
 export class LedTestComponent implements OnInit {
-  url = 'http://172.18.8.44:3000';
+  url = 'http://172.18.8.46:3000';
   model: any = {}; // 存储数据
 
   user: any = {}; // 用户列表
